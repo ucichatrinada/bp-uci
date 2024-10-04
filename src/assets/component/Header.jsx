70269915080cfcaee5eeec2472b4d3121b8f7256
Uci Chatrinada <ucichatrinada@gmail.com>
2024-10-05 02:15:01 +0700

    Edit Header Navbar

--- src/assets/component/Header.jsx
+++ src/assets/component/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Header.css';
-import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const handleLogout = () => {
+    // Menghapus status login dari localStorage
+    localStorage.removeItem('isLoggedIn');
+    alert('Anda telah logout.');
+    navigate('/login');
+  };
+
   return (
     <header>
       <nav>
@@ -11,24 +22,41 @@ const Header = () => {
         </div>
         <ul className="nav-links">
           <li>
-            <Link to ="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
             <Link to="/kelas">Kelas</Link>
-            </li>
-            <li>
-            <Link to="portofolio">Portofolio</Link>
-            </li>
-            <li>
-            <Link to="/gallery-cari-mua">Galerry Cari MUA</Link>
-            </li>
+          </li>
+          <li>
+            <Link to="/portofolio">Portofolio</Link>
+          </li>
+          <li>
+            <Link to="/gallery-cari-mua">Gallery Cari MUA</Link>
+          </li>
+
+          {isLoggedIn ? (
+            <>
+              <li>Halo, {user.nama}</li>
+              <li>
+                <button onClick={handleLogout} id="exit" className="logout-button">
+                  Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/login" id="signin">Login</Link>
+              </li>
+              <li>
+                <Link to="/registrasi" id="signup">Daftar</Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
   );
-}
+};
 
 export default Header;
